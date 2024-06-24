3af087a11f23ded07f070cedddb1e94cabe306ce
gabrycina <c.gabriele.info@gmail.com>
2024-06-24 22:34:26 +0100

    Move db url to env

--- app/db.py
+++ app/db.py
@@ -1,9 +1,17 @@
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Database setup
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("The DATABASE_URL environment variable is not set")
+
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = sqlalchemy.orm.declarative_base()
