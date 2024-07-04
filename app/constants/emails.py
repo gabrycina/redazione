1e534cf7b41bf52fc48646fc98d62fb7d9830e10
gabrycina <c.gabriele.info@gmail.com>
2024-07-04 21:50:24 +0100

    Fix drafter prompty and subject

--- app/constants/emails.py
+++ app/constants/emails.py
@@ -42,7 +42,7 @@ EMAIL_BODY = """
   <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Daily Redact 🚀</title>
+    <title>Daily Redact</title>
     <style media="all" type="text/css">
     /* -------------------------------------
     GLOBAL RESETS
--- app/constants/prompts.py
+++ app/constants/prompts.py
@@ -1,23 +1,17 @@
 DRAFTER_SYSTEM_PROMPT = """
-Your role is to select just articles' titles and then rank them based on user preferences.
+Your role is to provide a user with the most relevant articles and posts based on their preferences.
 
-You should look at titles that describe the content of an article. Here some examples:
-- The Science Behind Splashdown − How NASA And SpaceX Get Spacecraft Safely Back On Earth
-- MoviePass Became Profitable
-- AI's $600B Question
-- What I learned from looking at 900 most popular open source AI tools
-etc...
+Please follow the instructions below step by step:
 
+1) Select a maximum of 3 articles based on user preferences, if there are no articles or forum posts that match user preferences return zero articles and forum posts.
 
-Take a deep breath and select just titles that look like real articles titles, avoiding titles that look like "Terms of Conditions", "Home", profile pages, sections titles or any other title that looks like a link to navigate internally to a website.
+2) Check that the ones that you selected are not parts of the websites. If they are, remove them please.
 
-Select a maximum of 3 articles based on user preferences, if there are no articles that match user preferences do not select anything.
-Pay attention to user preferences, if an article it's not relevant do not select it.
-
-Output them as a json containing only strings that are EXACTLY EQUAL to the key of the article selected.
+Output them as a json containing only integers that are EXACTLY EQUAL to the key of the article selected.
 
 Example Input: {1:{"title":"...", "url":"..."}, 2:{"title":"...", "url":"..."}, ...},
-Example Output: {"ranked_data": [1, 4, ...]} 
+Example Output: {"ranked_data": [1, 4, ...]}
+
 """
 
 SUMMARIZER_SYSTEM_PROMPT = (
